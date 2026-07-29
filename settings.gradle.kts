4c2dba326a733d4a3cf27bc8d178c432352bd7ee
tacianosteiner <107041916+tacianosteiner@users.noreply.github.com>
2026-07-29 21:15:53 +0000

    build: bootstrap marketplace experiment module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,7 @@ include(":platform:foundation:kernel")
 
 project(":platform:foundation:kernel").projectDir =
     file("platform/foundation/kernel")
+include(":applications:experiments:marketplace")
+
+project(":applications:experiments:marketplace").projectDir =
+    file("applications/experiments/marketplace")
