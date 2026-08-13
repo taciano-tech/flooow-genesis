86d83b36b5ed9cf80cc6ac058f588ce8e87af63b
tacianosteiner <107041916+tacianosteiner@users.noreply.github.com>
2026-08-13 13:11:23 -0300

    test: expect candidate snapshot migration

--- applications/marketplace-operations-persistence-postgres/src/test/kotlin/io/flooow/marketplace/persistence/postgres/PostgresInventoryRiskAssessmentJournalTest.kt
+++ applications/marketplace-operations-persistence-postgres/src/test/kotlin/io/flooow/marketplace/persistence/postgres/PostgresInventoryRiskAssessmentJournalTest.kt
@@ -105,6 +105,9 @@ class PostgresInventoryRiskAssessmentJournalTest {
                         assertTrue(result.next())
                         assertEquals("011", result.getString("version"))
                         assertEquals(true, result.getBoolean("success"))
+                        assertTrue(result.next())
+                        assertEquals("012", result.getString("version"))
+                        assertEquals(true, result.getBoolean("success"))
                         assertTrue(!result.next())
                     }
                 }
